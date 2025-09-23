2cae8df4e1870b6d207de340371163cf9ef2fe87
bernardo <bernardo_nf@hotmail.com>
2025-09-23 10:39:03 -0300

    Controller e Service para busca de todos os endereços criados

--- customer-service/src/main/java/customer_service/controllers/AddressController.java
+++ customer-service/src/main/java/customer_service/controllers/AddressController.java
@@ -1,12 +1,14 @@
 package customer_service.controllers;
 
+import customer_service.dtos.AddressResponseDTO;
+import customer_service.dtos.CustomerResponseDTO;
 import customer_service.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
@@ -20,5 +22,17 @@ public class AddressController {
         addressService.deleteById(id);
         return ResponseEntity.noContent().build();
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressResponseDTO>> findAll() {
+
+        List<AddressResponseDTO> response = addressService.findAll();
+
+        if (response == null || response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
     }
 }
