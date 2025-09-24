82d41cc9c719e2ffb87c7f465b155594b5a741c0
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-09-24 16:11:40 -0300

    Mock de alguns dados no customer-service via import.sql

---
+++ customer-service/src/main/resources/import.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Rua das Flores', 123, 'Centro', 'Campinas', 'SP', '13000-000', '2023-12-01 08:30:00', '2024-01-15 10:00:00');
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Avenida Paulista', 1000, 'Bela Vista', 'São Paulo', 'SP', '01310-100', '2023-11-10 14:00:00', '2024-02-20 15:30:00');
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Rua XV de Novembro', 250, 'Centro', 'Curitiba', 'PR', '80020-310', '2023-10-05 11:00:00', '2024-03-01 09:00:00');
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Rua das Palmeiras', 45, 'Botafogo', 'Rio de Janeiro', 'RJ', '22270-070', '2023-09-20 16:00:00', '2024-04-10 12:00:00');
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Avenida Beira Mar', 567, 'Meireles', 'Fortaleza', 'CE', '60165-121', '2023-08-01 13:00:00', '2024-05-05 14:30:00');
+
+INSERT INTO tb_address (street, number, neighborhood, city, state, zip_code, on_create, on_update) VALUES ('Rua das Acácias', 321, 'Jardins', 'Belo Horizonte', 'MG', '30112-020', '2023-07-15 09:45:00', '2024-06-01 11:45:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('João Silva', '39053344705', '1990-05-20', 3500.00, '12345-6', 'Masculino', 'Solteiro', 'Analista de Sistemas', 1, '2023-12-01 08:30:00', '2024-01-15 10:00:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('Maria Oliveira', '84859846001', '1985-09-15', 5000.00, '65432-1', 'Feminino', 'Casada', 'Engenheira Civil', 2, '2023-11-10 14:00:00', '2024-02-20 15:30:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('Carlos Pereira', '62648716002', '1978-12-02', 4200.00, '11223-4', 'Masculino', 'Casado', 'Professor', 3, '2023-10-05 11:00:00', '2024-03-01 09:00:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('Ana Costa', '98765432100', '1992-07-10', 2800.00, '99887-6', 'Feminino', 'Solteira', 'Designer Gráfica', 4, '2023-09-20 16:00:00', '2024-04-10 12:00:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('Paulo Souza', '74562342000', '1980-03-25', 6100.00, '22334-5', 'Masculino', 'Divorciado', 'Médico', 5, '2023-08-01 13:00:00', '2024-05-05 14:30:00');
+
+INSERT INTO tb_customer (name, cpf, date_ofbirth, income, bank_account, gender, marital_status, job_title, address_id, on_create, on_update) VALUES ('Juliana Almeida', '48844278000', '1995-11-30', 3200.00, '55667-8', 'Feminino', 'Solteira', 'Advogada', 6, '2023-07-15 09:45:00', '2024-06-01 11:45:00');
