05025e001e73daa18d2849700cd75418497d3ece
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-09-24 11:22:17 -0300

    Criado entidade Analytic

--- analytic-service/src/main/java/analytic_service/AnalyticServiceApplication.java
+++ analytic-service/src/main/java/analytic_service/AnalyticServiceApplication.java
@@ -2,7 +2,9 @@ package analytic_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AnalyticServiceApplication {
 
