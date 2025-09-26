b1dc8140f417b23bc309136f9daba7be524f7d3c
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-09-26 16:12:06 -0300

    Criado Query na camada repository para busca do historico no ultimo ano

--- history-service/src/main/java/history_service/HistoryServiceApplication.java
+++ history-service/src/main/java/history_service/HistoryServiceApplication.java
@@ -2,8 +2,10 @@ package history_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HistoryServiceApplication {
 
 	public static void main(String[] args) {
