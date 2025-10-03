af7b926ba364ebf7626ed6f2602e3f2db65c0d15
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-10-03 15:44:50 -0300

    Criado DTO, Service e Controller no auth-service

---
+++ auth-service/src/main/java/auth_service/services/ApiKeyService.java
@@ -0,0 +1,12 @@
+package auth_service.services;
+
+import auth_service.dtos.ApiKeyValidationDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiKeyService {
+    public ApiKeyValidationDTO validate(String apiName, String apiKey) {
+
+        return null;
+    }
+}
