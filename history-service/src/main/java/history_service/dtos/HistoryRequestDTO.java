83dd8c154a48353a771d7e9919ff31fac58e775d
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-10-03 22:11:35 -0300

    Microserviço history documentado com Swagger

--- history-service/src/main/java/history_service/dtos/HistoryRequestDTO.java
+++ history-service/src/main/java/history_service/dtos/HistoryRequestDTO.java
@@ -1,10 +1,15 @@
 package history_service.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Map;
 
 public class HistoryRequestDTO {
 
+    @Schema(description = "ID do cliente", example = "6")
     private Long customerId;
+    @Schema(description = "Payload de evento",
+            example = "{\"eventType\": \"ATRASO\", \"value\": 4103.18, \"days_overdue\": 4, \"occurence_date\": \"2024-12-22\"}")
     private Map<String, Object> payload;
 
     public HistoryRequestDTO() {
